3d065d231ef00fa9b03ad56ab96ec508705cb75e
Alexander Almengor <alexanderalmengor1@gmail.com>
2020-11-09 09:12:07 -0500

    Added ChromeDriverManager

--- features/environment.py
+++ features/environment.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
 
@@ -6,11 +7,12 @@ def before_all(context):
 
     chromeOptions = Options()
 
-    #chromeOptions.headless = False
+    # chromeOptions.headless = False
     chromeOptions.headless = True
 
-    context.driver = webdriver.Chrome(executable_path='./chromedriver',
-                                      chrome_options=chromeOptions)
+    context.driver = webdriver.Chrome(ChromeDriverManager().install())
+    # context.driver = webdriver.Chrome(executable_path='./chromedriver',
+    #                                   chrome_options=chromeOptions)
     context.driver.implicitly_wait(10)
 
 
