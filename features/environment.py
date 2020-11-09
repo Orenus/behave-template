4cd529946df203abf30fe4adc09cafedfae4ead4
Cristopher <cristophermoreno92@gmail.com>
2020-11-08 21:17:24 -0500

    login

--- features/environment.py
+++ features/environment.py
@@ -1,10 +1,20 @@
-from browser import Browser
+#from browser import Browser
 from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def before_all(context):
-    context.browser = Browser()
+    chromeOptions = Options()
+
+    chromeOptions.headless = False
+    #chromeOptions.headless = True
+
+    context.driver = webdriver.Chrome(
+        executable_path='chromedriver',
+        chrome_options=chromeOptions)
+    context.driver.implicitly_wait(10)
 
 
 def after_all(context):
-    context.browser.close()
+    context.driver.close()
--- features/steps/ica_login.py
+++ features/steps/ica_login.py
@@ -1,18 +1,27 @@
-from behave import given, when, then
-from selenium.webdriver.support.ui import WebDriverWait
+from behave import given, then, when
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+import os
+from dotenv import load_dotenv
+load_dotenv('.env')
 
 
 @given('se navega a "{site}"')
-def step(context,site):
-    context.browser.visit(site)
+def step(context, site):
+    context.driver.get(site)
     pass
 
 
 @when('se accede a la pagina principal')
 def step(context):
-    # search_field = context.find_element(By.XPATH, 'id("twotabsearchtextbox")')
-    # search_field.send_keys(product)
+
+    print(context)
+    SECRET_KEY = os.getenv('SECRET_00')
+
+    box = context.driver.find_element_by_xpath(
+        '//*[@id="root"]/div/div/div/div/div/div/div/div/div/form/div[1]/input')
+    box.send_keys('joshua')
     pass
