dae5edf4f544af5ea89aa0c155b80db9fa1a38dd
Cristopher <cristophermoreno92@gmail.com>
2020-11-09 03:26:31 -0500

    driver

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,16 @@
 # Python base image 
-FROM python:3.8
+FROM python:3.7
+
+USER root
 
 LABEL MAINTAINER="The Simple Company"
 
 # Installing python dependencies
 COPY ./requirements.txt ./requirements.txt
 RUN python3 -m pip install -r requirements.txt
-RUN python3 -m pip install behave
+
+#RUN sbase install chromedriver latest
+#RUN mv /usr/local/lib/python3.7/site-packages/seleniumbase/drivers/chromedriver /usr/bin/chromedriver
 
 # Pushing the code inside the container
 COPY . .
--- features/environment.py
+++ features/environment.py
@@ -3,14 +3,14 @@ from selenium.webdriver.chrome.options import Options
 
 
 def before_all(context):
+
     chromeOptions = Options()
 
     #chromeOptions.headless = False
     chromeOptions.headless = True
 
-    context.driver = webdriver.Chrome(
-        #executable_path='chromedriver',
-        chrome_options=chromeOptions)
+    context.driver = webdriver.Chrome(executable_path='./chromedriver',
+                                      chrome_options=chromeOptions)
     context.driver.implicitly_wait(10)
 
 
